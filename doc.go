68980b2203f1023b07e4f07925f79349f22aaab0
caixw <xnotepad@gmail.com>
2015-01-20 00:13:22 +0800

    更新文档内容

--- doc.go
+++ doc.go
@@ -2,25 +2,26 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
-// orm提供了一个相对统一的数据库操作，目前内置了对部分数据库的支持，
-// 用户也可以自行实现core.Dialect接口，来实现特定数据库的支持。
+// orm提供了一个相对统一和方便的数据库操作。
 //
-// 支持的数据库：
-//  1. sqlite3: github.com/mattn/go-sqlite3
-//  2. mysql:   github.com/go-sql-driver/mysql
-//  3. postgres:github.com/lib/pq
-// 其它数据库，用户可以通过实现orm/core.Dialect接口，
-// 然后调用core.Register()注册来实现支持。
+// 目前内置了对以下数据库的支持：
+//  1. sqlite3:  github.com/mattn/go-sqlite3
+//  2. mysql:    github.com/go-sql-driver/mysql
+//  3. postgres: github.com/lib/pq
+// 其它数据库，用户可以通过实现orm/core.Dialect接口，来实现相应的支持。
+// 具体操作参照后面的如何实现Dialect章节。
+//
+//
 //
 // 初始化：
 //
 // 默认情况下，orm包并不会加载任何数据库的实例。所以想要用哪个数据库，需要手动初始化：
 //  import (
-//      github.com/issue9/orm/core     // core.Register(...)注册dialect
+//      github.com/issue9/orm/core
 //      _ github.com/mattn/go-sqlite3  // 加载数据库驱动
 //  )
 //
-//  // 向orm包注册dialect
+//  // 注册dialect
 //  core.Register("sqlite3", &dialect.Sqlite3{})
 //
 //  // 初始化一个Engine，表前缀为prefix_
@@ -29,7 +30,11 @@
 //  // 另一个Engine
 //  db2 := orm.New("sqlite3", "./db2", "db2", "db2_")
 //
+//
+//
 // Model:
+//
+// orm包通过struct tag来描述model在数据库中的结构。大概格式如下：
 //  type User struct {
 //      Id          int64      `orm:"name(id);ai;"`
 //      FirstName   string     `orm:"name(first_name);index(index_name)"`
@@ -40,8 +45,8 @@
 //  func(u *User) Meta() string {
 //      return "name(user);engine(innodb);charset(utf-8)"
 //  }
-// 通过struct tag可以直接将一个结构体定义为一个数据表结构，
-// struct tag的语法结构，如上面代码所示，目前支持以下的struct tag：
+//
+// 目前支持以下的struct tag：
 //
 //  name(fieldName): 将当前的字段映射到数据表中的fieldName字段。
 //
@@ -63,9 +68,8 @@
 //  default(value): 指定默认值。相当于定义表结构时的DEFAULT。
 //
 //  fk(fk_name,refTable,refColName,updateRule,deleteRule):
-//  定义物理外键，最少需要指定fk_name,refTabl,refColName三个值。
-//  分别对应约束名，引用的表和引用的字段，updateRule,deleteRule，
-//  在不指定的情况下，使用数据库的默认值。
+//  定义物理外键，最少需要指定fk_name,refTabl,refColName三个值。分别对应约束名，
+//  引用的表和引用的字段，updateRule,deleteRule，在不指定的情况下，使用数据库的默认值。
 //
 // 关于core.Metaer接口。
 //
@@ -76,13 +80,14 @@
 // 在core.Metaer中除了可以指定name(table_name)和check(name,expr)两个属性之外，
 // 还可指定一些自定义的属性，这些属性都将会被保存到Model.Meta中。
 //
-// 当然在go中receive区分值类型和指针类型，所以指定接口时，需要注意这个情况。
+// NOTE:一定要注意receive的类型是值还是指针。
 //
 // 约束名：
 //
 // index,unique,check,fk都是可以指定约束名的，在表中，约束名必须是唯一的，
 // 即便是不同类型的约束，比如已经有一个unique的约束名叫作name，
 // 那么其它类型的约束，就不能再取这个名称了。
+//
 //
 //
 // 如何使用：
@@ -123,6 +128,12 @@
 // 默认的Engine是不支持事务的，若需要事务支持，则需要调用Engine.Begin()
 // 返回事务对象Tx，当然并不是所有的数据库都支持事务操作的。
 // Tx拥有与Engine相似的接口。
+//
+//
+// 如何实现自定义Dialect:
+//
+// 实现core.Dialect接口，需要使用时，加载数据库驱动，然后向core.Register()函数注册实例，
+// 即可正常使用。具体的实现可以参照dialect子包的相关代码。
 package orm
 
 // 数据表的更改，涉及到很多方面：
@@ -130,7 +141,7 @@ package orm
 //  2.字段类型更改，且不兼容旧类型；
 //  3.名称加了特殊修饰符：比如sqlite中的[group]与`group`为同一字段名，但表示形式不同；
 // 所有这一切都让UpgradeTable()这一功能的实现变得很糟糕，
-// 在没有比较完美的方法之前，准备先去掉这个功能。
+// 在没有比较完美的方法之前，不准备实现这个功能。
 
 // 版本号
-const Version = "0.9.14.150119"
+const Version = "0.9.15.150119"
