20141861464941eb886228d5ae9b9fd803b553d5
caixw <xnotepad@gmail.com>
2015-04-01 22:46:12 +0800

    添加部分builder包的测试用例

--- builder/where_test.go
+++ builder/where_test.go
@@ -22,32 +22,53 @@ func TestSQL_Where(t *testing.T) {
 
 	wont := "select c1 from user where(id=@id) and(group=1) or(name like @name)"
 	chkSQLEqual(a, wont, sql.selectSQL())
+	a.False(sql.HasErrors())
 
 	// In
 	sql.Reset().
 		Columns("c1").
 		Table("user").
-		In("c1", 1, 2, "3")
+		In("c1", 1, 2, "3").
+		OrIn("c2", 4, 5, "6")
 
-	wont = "select c1 from user where(c1 in(1,2,'3'))"
+	wont = "select c1 from user where(c1 in(1,2,'3')) or (c2 in(4,5,'6'))"
 	chkSQLEqual(a, wont, sql.selectSQL())
+	a.False(sql.HasErrors())
 
 	// between
 	sql.Reset().
 		Columns("c1").
 		Table("user").
-		Between("c1", 5, 7)
+		Between("c1", 5, "@end").
+		OrBetween("c2", 7, 8)
 
-	wont = "SELECT c1 from user where (c1 between 5 and 7)"
+	wont = "SELECT c1 from user where (c1 between 5 and @end) or (c2 between 7 and 8)"
 	chkSQLEqual(a, wont, sql.selectSQL())
+	a.False(sql.HasErrors())
 
 	// is null
 	sql.Reset().
 		Columns("*").
 		Table("user").
 		IsNotNull("c1").
-		OrIsNull("c2")
+		OrIsNotNull("c2").
+		IsNull("c3").
+		OrIsNull("c4")
 
-	wont = "SELECT * FROM user where(c1 is not null) or (c2 is null)"
+	wont = "SELECT * FROM user where(c1 is not null) or (c2 is not null) and (c3 is null) OR (c4 is null)"
 	chkSQLEqual(a, wont, sql.selectSQL())
+	a.False(sql.HasErrors())
+
+	// in参数错误
+	sql.Reset().In("c1")
+	a.True(sql.HasErrors())
+
+	// where build 参数错误
+	sql.Reset()
+	a.False(sql.HasErrors())
+	sql.whereBuild(3, "id=@id") // 第一次忽略op参数
+	a.False(sql.HasErrors())
+	sql.whereBuild(3, "id=@id") // 第二次错误的op参数，会报错
+	a.True(sql.HasErrors())
+
 }
