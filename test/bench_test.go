bb6b6da60dff78064cff49eec9979ab55c91125b
caixw <xnotepad@gmail.com>
2015-05-28 11:10:56 +0800

    添加两段where测试代码

--- test/bench_test.go
+++ test/bench_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/issue9/assert"
 )
 
-// BenchmarkDB_Insert	    5000	    234033 ns/op
+// mysql: BenchmarkDB_Insert	    5000	    234033 ns/op
 func BenchmarkDB_Insert(b *testing.B) {
 	a := assert.New(b)
 
@@ -33,7 +33,7 @@ func BenchmarkDB_Insert(b *testing.B) {
 	}
 }
 
-// BenchmarkDB_MultInsert	      50	  23436852 ns/op
+// mysql: BenchmarkDB_MultInsert	      50	  23436852 ns/op
 func BenchmarkDB_MultInsert(b *testing.B) {
 	a := assert.New(b)
 
@@ -59,7 +59,7 @@ func BenchmarkDB_MultInsert(b *testing.B) {
 	}
 }
 
-// BenchmarkDB_Update	    5000	    290209 ns/op
+// mysql: BenchmarkDB_Update	    5000	    290209 ns/op
 func BenchmarkDB_Update(b *testing.B) {
 	a := assert.New(b)
 
@@ -85,7 +85,7 @@ func BenchmarkDB_Update(b *testing.B) {
 	}
 }
 
-// BenchmarkDB_MultUpdate	      50	  28662471 ns/op
+// mysql: BenchmarkDB_MultUpdate	      50	  28662471 ns/op
 func BenchmarkDB_MultUpdate(b *testing.B) {
 	a := assert.New(b)
 
@@ -119,7 +119,7 @@ func BenchmarkDB_MultUpdate(b *testing.B) {
 	}
 }
 
-// BenchmarkDB_Select	   10000	    181897 ns/op
+// mysql: BenchmarkDB_Select	   10000	    181897 ns/op
 func BenchmarkDB_Select(b *testing.B) {
 	a := assert.New(b)
 
@@ -144,7 +144,7 @@ func BenchmarkDB_Select(b *testing.B) {
 	}
 }
 
-// BenchmarkDB_MultSelect	     100	  18494929 ns/op
+// mysql: BenchmarkDB_MultSelect	     100	  18494929 ns/op
 func BenchmarkDB_MultSelect(b *testing.B) {
 	a := assert.New(b)
 
@@ -175,5 +175,70 @@ func BenchmarkDB_MultSelect(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		a.NotError(db.Select(ms...))
+	}
+}
+
+// mysql: BenchmarkDB_WhereUpdate	   10000	    169674 ns/op
+func BenchmarkDB_WhereUpdate(b *testing.B) {
+	a := assert.New(b)
+
+	m := &bench{
+		Name: "name",
+		Pass: "pass",
+		Site: "http://www.github.com/issue9/orm",
+	}
+
+	db := newDB(a)
+	defer func() {
+		db.Drop(&bench{})
+		closeDB(a)
+	}()
+
+	// 构造数据
+	a.NotError(db.Create(&bench{}))
+	a.NotError(db.Insert(m))
+
+	for i := 0; i < b.N; i++ {
+		a.NotError(db.Where("id=?", i+1).Table("#bench").Update(true, map[string]interface{}{
+			"name": "n1",
+			"pass": "p1",
+			"site": "s1",
+		}))
+	}
+}
+
+// mysql: BenchmarkDB_WhereSelect	   10000	    182159 ns/op
+func BenchmarkDB_WhereSelect(b *testing.B) {
+	a := assert.New(b)
+
+	ms := make([]interface{}, 0, 100)
+	for i := 0; i < cap(ms); i++ {
+		ms = append(ms, &bench{
+			Name: "name",
+			Pass: "pass",
+			Site: "http://www.github.com/issue9/orm",
+		})
+	}
+
+	db := newDB(a)
+	defer func() {
+		db.Drop(&bench{})
+		closeDB(a)
+	}()
+
+	// 构造数据
+	a.NotError(db.Create(&bench{}))
+	a.NotError(db.Insert(ms...))
+
+	models := make([]*bench, 0, 100)
+	for i := 0; i < cap(models); i++ {
+		models = append(models, &bench{
+			Name: "name",
+			Pass: "pass",
+			Site: "http://www.github.com/issue9/orm",
+		})
+	}
+	for i := 0; i < b.N; i++ {
+		a.NotError(db.Where("id>?", i).Table("#bench").Select(true, models))
 	}
 }
