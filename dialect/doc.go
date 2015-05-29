b8df4973e4679b565f7869be17413bfdaf3f483b
caixw <xnotepad@gmail.com>
2015-05-29 19:23:36 +0800

    为dialect包添加一个简单的说明文档

---
+++ dialect/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2015 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+// 提供了部分数据库对orm.Dialect接口的实现。
+package dialect
