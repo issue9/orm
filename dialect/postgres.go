a89768dd267645958613b6343f9255ae73a984de
caixw <xnotepad@gmail.com>
2015-10-07 21:40:59 +0800

    float类型需要一个两个长度标记

--- dialect/postgres.go
+++ dialect/postgres.go
@@ -154,6 +154,9 @@ func (p *postgres) sqlType(buf *bytes.Buffer, col *forward.Column) error {
 			buf.WriteString("BIGINT")
 		}
 	case reflect.Float32, reflect.Float64:
+		if col.Len1 == 0 || col.Len2 == 0 {
+			return errors.New("请指定长度")
+		}
 		buf.WriteString(fmt.Sprintf("DOUBLE(%d,%d)", col.Len1, col.Len2))
 	case reflect.String:
 		if col.Len1 == -1 || col.Len1 > 65533 {
