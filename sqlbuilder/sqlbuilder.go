0fddde1499be57cfa55f059d0e742b59fe341602
caixw <xnotepad@gmail.com>
2016-05-12 00:19:46 +0800

    [sqlbuilder]添加部分执行函数

--- sqlbuilder/sqlbuilder.go
+++ sqlbuilder/sqlbuilder.go
@@ -9,6 +9,7 @@ import (
 	"database/sql"
 	"errors"
 
+	"github.com/issue9/orm/fetch"
 	"github.com/issue9/orm/forward"
 )
 
@@ -57,148 +58,148 @@ func New(e forward.Engine) *SQLBuilder {
 }
 
 // 重置所有的数据为初始值，这样可以重复利用该 SQLBuilder 对象。
-func (sql *SQLBuilder) Reset() *SQLBuilder {
-	sql.buffer.Reset()
-	sql.args = sql.args[:0]
-	sql.flag = 0
-	sql.errors = sql.errors[:0]
+func (sb *SQLBuilder) Reset() *SQLBuilder {
+	sb.buffer.Reset()
+	sb.args = sb.args[:0]
+	sb.flag = 0
+	sb.errors = sb.errors[:0]
 
-	return sql
+	return sb
 }
 
-func (sql *SQLBuilder) isSetFlag(flag int8) bool {
-	return sql.flag&flag > 0
+func (sb *SQLBuilder) isSetFlag(flag int8) bool {
+	return sb.flag&flag > 0
 }
 
-func (sql *SQLBuilder) setFlag(flag int8) {
-	sql.flag |= flag
+func (sb *SQLBuilder) setFlag(flag int8) {
+	sb.flag |= flag
 }
 
 // 是否在构建过程中触发错误信息。
 //
 // NOTE: 在构建完 SQL 语句，准备执行数据库操作之前，
 // 都应该调用此函数确认是否存在错误。
-func (sql *SQLBuilder) HasError() bool {
-	return len(sql.errors) > 0
+func (sb *SQLBuilder) HasError() bool {
+	return len(sb.errors) > 0
 }
 
 // 返回所有的错误内容。
-func (sql *SQLBuilder) Errors() []error {
-	return sql.errors
+func (sb *SQLBuilder) Errors() []error {
+	return sb.errors
 }
 
-func (sql *SQLBuilder) WriteByte(c byte) *SQLBuilder {
-	err := sql.buffer.WriteByte(c)
+func (sb *SQLBuilder) WriteByte(c byte) *SQLBuilder {
+	err := sb.buffer.WriteByte(c)
 	if err != nil {
-		sql.errors = append(sql.errors, err)
+		sb.errors = append(sb.errors, err)
 	}
 
-	return sql
+	return sb
 }
 
-func (sql *SQLBuilder) WriteString(s string) *SQLBuilder {
-	_, err := sql.buffer.WriteString(s)
+func (sb *SQLBuilder) WriteString(s string) *SQLBuilder {
+	_, err := sb.buffer.WriteString(s)
 	if err != nil {
-		sql.errors = append(sql.errors, err)
+		sb.errors = append(sb.errors, err)
 	}
 
-	return sql
+	return sb
 }
 
 // 去掉尾部的 n 个字符。
-func (sql *SQLBuilder) TruncateLast(n int) *SQLBuilder {
-	sql.buffer.Truncate(sql.buffer.Len() - n)
-	return sql
+func (sb *SQLBuilder) TruncateLast(n int) *SQLBuilder {
+	sb.buffer.Truncate(sb.buffer.Len() - n)
+	return sb
 }
 
 // 启动一个 DELETE 语句。
-func (sql *SQLBuilder) Delete(table string) *SQLBuilder {
-	return sql.WriteString("DELETE FROM ").WriteString(table)
+func (sb *SQLBuilder) Delete(table string) *SQLBuilder {
+	return sb.WriteString("DELETE FROM ").WriteString(table)
 }
 
 // 启动一个 SELECT 语句，并指定列名。可多次调用。
-func (sql *SQLBuilder) Select(cols ...string) *SQLBuilder {
-	if !sql.isSetFlag(flagColumn) {
-		sql.WriteString("SELECT ")
-		sql.setFlag(flagColumn)
+func (sb *SQLBuilder) Select(cols ...string) *SQLBuilder {
+	if !sb.isSetFlag(flagColumn) {
+		sb.WriteString("SELECT ")
+		sb.setFlag(flagColumn)
 	}
 
 	for _, col := range cols {
-		sql.WriteString(col)
-		sql.WriteByte(',')
+		sb.WriteString(col)
+		sb.WriteByte(',')
 	}
-	return sql.TruncateLast(1)
+	return sb.TruncateLast(1)
 }
 
 // 启动一个 INSERT 语句。
-func (sql *SQLBuilder) Insert(table string) *SQLBuilder {
-	return sql.WriteString("INSERT INTO ").WriteString(table)
+func (sb *SQLBuilder) Insert(table string) *SQLBuilder {
+	return sb.WriteString("INSERT INTO ").WriteString(table)
 }
 
 // 启动一个 UPDATE 语句。
-func (sql *SQLBuilder) Update(table string) *SQLBuilder {
-	return sql.WriteString("UPDATE ").WriteString(table)
+func (sb *SQLBuilder) Update(table string) *SQLBuilder {
+	return sb.WriteString("UPDATE ").WriteString(table)
 }
 
 // 拼接表名字符串。当调用 Select() 之后，此方法用于指定表名。
-func (sql *SQLBuilder) From(table string) *SQLBuilder {
-	sql.WriteString(table)
-	return sql
+func (sb *SQLBuilder) From(table string) *SQLBuilder {
+	sb.WriteString(table)
+	return sb
 }
 
 // 构建 WHERE 语句，op 只能是 AND 或是 OR
-func (sql *SQLBuilder) where(op string, cond string, args ...interface{}) *SQLBuilder {
-	if !sql.isSetFlag(flagWhere) {
-		sql.setFlag(flagWhere)
+func (sb *SQLBuilder) where(op string, cond string, args ...interface{}) *SQLBuilder {
+	if !sb.isSetFlag(flagWhere) {
+		sb.setFlag(flagWhere)
 		op = " WHERE "
 	}
 
-	sql.WriteString(op)
-	sql.WriteString(cond)
-	sql.args = append(sql.args, args...)
+	sb.WriteString(op)
+	sb.WriteString(cond)
+	sb.args = append(sb.args, args...)
 
-	return sql
+	return sb
 }
 
 // And 的别名。
-func (sql *SQLBuilder) Where(cond string, args ...interface{}) *SQLBuilder {
-	return sql.And(cond, args...)
+func (sb *SQLBuilder) Where(cond string, args ...interface{}) *SQLBuilder {
+	return sb.And(cond, args...)
 }
 
-func (sql *SQLBuilder) And(cond string, args ...interface{}) *SQLBuilder {
-	return sql.where(" AND ", cond, args...)
+func (sb *SQLBuilder) And(cond string, args ...interface{}) *SQLBuilder {
+	return sb.where(" AND ", cond, args...)
 }
 
-func (sql *SQLBuilder) Or(cond string, args ...interface{}) *SQLBuilder {
-	return sql.where(" OR ", cond, args...)
+func (sb *SQLBuilder) Or(cond string, args ...interface{}) *SQLBuilder {
+	return sb.where(" OR ", cond, args...)
 }
 
-func (sql *SQLBuilder) orderBy(order, col string) *SQLBuilder {
-	if !sql.isSetFlag(flagOrder) {
-		sql.setFlag(flagOrder)
-		sql.WriteString(" ORDER BY ")
+func (sb *SQLBuilder) orderBy(order, col string) *SQLBuilder {
+	if !sb.isSetFlag(flagOrder) {
+		sb.setFlag(flagOrder)
+		sb.WriteString(" ORDER BY ")
 	} else {
-		sql.WriteByte(',')
+		sb.WriteByte(',')
 	}
 
-	sql.WriteString(col)
-	sql.WriteString(order)
+	sb.WriteString(col)
+	sb.WriteString(order)
 
-	return sql
+	return sb
 }
 
-func (sql *SQLBuilder) Desc(col string) *SQLBuilder {
-	return sql.orderBy(" DESC ", col)
+func (sb *SQLBuilder) Desc(col string) *SQLBuilder {
+	return sb.orderBy(" DESC ", col)
 }
 
-func (sql *SQLBuilder) Asc(col string) *SQLBuilder {
-	return sql.orderBy(" ASC ", col)
+func (sb *SQLBuilder) Asc(col string) *SQLBuilder {
+	return sb.orderBy(" ASC ", col)
 }
 
-func (sql *SQLBuilder) Limit(limit, offset int) *SQLBuilder {
-	vals, err := sql.engine.Dialect().LimitSQL(sql.buffer, limit, offset)
+func (sb *SQLBuilder) Limit(limit, offset int) *SQLBuilder {
+	vals, err := sb.engine.Dialect().LimitSQL(sb.buffer, limit, offset)
 	if err != nil {
-		sql.errors = append(sql.errors, err)
+		sb.errors = append(sb.errors, err)
 	}
 
 	args := make([]interface{}, 0, 2)
@@ -206,90 +207,169 @@ func (sql *SQLBuilder) Limit(limit, offset int) *SQLBuilder {
 		args = append(args, val)
 	}
 
-	sql.args = append(sql.args, args...)
+	sb.args = append(sb.args, args...)
 
-	return sql
+	return sb
 }
 
 // 指定插入数据时的列名
-func (sql *SQLBuilder) Keys(keys ...string) *SQLBuilder {
-	sql.WriteByte('(')
+func (sb *SQLBuilder) Keys(keys ...string) *SQLBuilder {
+	sb.WriteByte('(')
 	for _, key := range keys {
-		sql.WriteString(key)
-		sql.WriteByte(',')
+		sb.WriteString(key)
+		sb.WriteByte(',')
 	}
-	sql.TruncateLast(1)
-	sql.WriteByte(')')
+	sb.TruncateLast(1)
+	sb.WriteByte(')')
 
-	return sql
+	return sb
 }
 
 // 指定插入的数据，需要与 Keys 中的名称一一对应。
-func (sql *SQLBuilder) Values(vals ...interface{}) *SQLBuilder {
-	if !sql.isSetFlag(flagValues) {
-		sql.WriteString("VALUES(")
-		sql.setFlag(flagValues)
+func (sb *SQLBuilder) Values(vals ...interface{}) *SQLBuilder {
+	if !sb.isSetFlag(flagValues) {
+		sb.WriteString("VALUES(")
+		sb.setFlag(flagValues)
 	} else {
-		sql.WriteString(",(")
+		sb.WriteString(",(")
 	}
 
 	for _, v := range vals {
-		sql.WriteString("?,")
-		sql.args = append(sql.args, v)
+		sb.WriteString("?,")
+		sb.args = append(sb.args, v)
 	}
-	sql.TruncateLast(1)
+	sb.TruncateLast(1)
 
-	sql.WriteByte(')')
+	sb.WriteByte(')')
 
-	return sql
+	return sb
 }
 
 // 指定需要更新的数据
-func (sql *SQLBuilder) Set(k string, v interface{}) *SQLBuilder {
-	if !sql.isSetFlag(flagSet) {
-		sql.WriteString(" SET ")
-		sql.setFlag(flagSet)
+func (sb *SQLBuilder) Set(k string, v interface{}) *SQLBuilder {
+	if !sb.isSetFlag(flagSet) {
+		sb.WriteString(" SET ")
+		sb.setFlag(flagSet)
 	} else {
-		sql.WriteByte(',')
+		sb.WriteByte(',')
 	}
 
-	sql.WriteString(k)
-	sql.WriteString("=?")
+	sb.WriteString(k)
+	sb.WriteString("=?")
 
-	sql.args = append(sql.args, v)
+	sb.args = append(sb.args, v)
 
-	return sql
+	return sb
 }
 
-func (sql *SQLBuilder) Join(typ, table, on string) *SQLBuilder {
-	sql.WriteString(typ)
-	sql.WriteString(" JOIN ")
-	sql.WriteString(table)
-	sql.WriteByte(' ')
-	sql.WriteString(on)
+func (sb *SQLBuilder) Join(typ, table, on string) *SQLBuilder {
+	sb.WriteString(typ)
+	sb.WriteString(" JOIN ")
+	sb.WriteString(table)
+	sb.WriteByte(' ')
+	sb.WriteString(on)
 
-	return sql
+	return sb
 }
 
 // 返回 SQL 语句和其对应的值。
-func (sql *SQLBuilder) String() (string, []interface{}, error) {
-	if sql.HasError() {
+func (sb *SQLBuilder) String() (string, []interface{}, error) {
+	if sb.HasError() {
 		return "", nil, ErrHasErrors
 	}
 
-	return sql.buffer.String(), sql.args, nil
+	return sb.buffer.String(), sb.args, nil
 }
 
-// 返回预编译的 实例及对应的值。
-func (sql *SQLBuilder) Prepare() (*sql.Stmt, []interface{}, error) {
-	if sql.HasError() {
+// 返回预编译的实例及对应的值。
+func (sb *SQLBuilder) Prepare() (*sql.Stmt, []interface{}, error) {
+	if sb.HasError() {
 		return nil, nil, ErrHasErrors
 	}
 
-	stmt, err := sql.engine.Prepare(true, sql.buffer.String())
+	stmt, err := sb.engine.Prepare(true, sb.buffer.String())
 	if err != nil {
 		return nil, nil, err
 	}
 
-	return stmt, sql.args, nil
+	return stmt, sb.args, nil
+}
+
+func (sb *SQLBuilder) Exec(replace bool) (sql.Result, error) {
+	query, vals, err := sb.String()
+	if err != nil {
+		return nil, err
+	}
+
+	return sb.engine.Exec(replace, query, vals...)
+}
+
+func (sb *SQLBuilder) Query(replace bool) (*sql.Rows, error) {
+	query, vals, err := sb.String()
+	if err != nil {
+		return nil, err
+	}
+
+	return sb.engine.Query(replace, query, vals...)
+}
+
+// 返回符合当前条件的所有记录。
+//
+// replace，是否需将对语句的占位符进行替换。
+func (sb *SQLBuilder) QueryMap(replace bool) ([]map[string]interface{}, error) {
+	rows, err := sb.Query(replace)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	return fetch.Map(false, rows)
+}
+
+// 返回符合当前条件的所有记录，功能与 QueryMap 相同，但 map 中的值全为字符串类型。
+//
+// replace，是否需将对语句的占位符进行替换。
+func (sb *SQLBuilder) QueryMapString(replace bool) ([]map[string]string, error) {
+	rows, err := sb.Query(replace)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	return fetch.MapString(false, rows)
+}
+
+// 将符合当前条件的所有记录依次写入 objs 中。
+//
+// replace，是否需将对语句的占位符进行替换。
+func (sb *SQLBuilder) QueryObj(replace bool, objs interface{}) (int, error) {
+	rows, err := sb.Query(replace)
+	if err != nil {
+		return 0, err
+	}
+	defer rows.Close()
+
+	return fetch.Obj(objs, rows)
+}
+
+// 返回符合条件的记录中的某一列值。
+func (sb *SQLBuilder) QueryColumn(replace bool, col string) ([]interface{}, error) {
+	rows, err := sb.Query(replace)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	return fetch.Column(false, col, rows)
+}
+
+// 返回符合条件的记录中的某一列值，功能与 QueryColumn 相同，但返回的值均为字符串类型。
+func (sb *SQLBuilder) QueryColumnString(replace bool, col string) ([]string, error) {
+	rows, err := sb.Query(replace)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	return fetch.ColumnString(false, col, rows)
 }
