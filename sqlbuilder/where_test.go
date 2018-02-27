af40e38c78bd0be156dbdbea368c6a7bb57ae78b
caixw <xnotepad@gmail.com>
2018-02-28 01:30:52 +0800

    添加测试

--- sqlbuilder/where_test.go
+++ sqlbuilder/where_test.go
@@ -4,4 +4,34 @@
 
 package sqlbuilder
 
+import (
+	"testing"
+
+	"github.com/issue9/assert"
+)
+
 var _ SQL = &where{}
+
+func TestWhere(t *testing.T) {
+	a := assert.New(t)
+	w := newWhere()
+	a.NotNil(w)
+
+	w.and("id=?", 1)
+	w.and("name like ?", "name")
+	w.or("type=?", 5)
+	sql, args, err := w.SQL()
+	a.NotError(err).NotNil(args).NotEmpty(sql)
+	a.Equal(args, []interface{}{1, "name", 5})
+	chkSQLEqual(a, sql, "where id=? and name like ? or type=?")
+
+	w.Reset()
+	a.Equal(0, w.buffer.Len())
+	a.Equal(0, len(w.args))
+
+	w.and("id=?", 5)
+	sql, args, err = w.SQL()
+	a.NotError(err).NotNil(args).NotEmpty(sql)
+	a.Equal(args, []interface{}{5})
+	chkSQLEqual(a, sql, "where id=?")
+}
