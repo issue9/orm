3fe2ed217381800ff43360fba52155bb73371ae3
caixw <xnotepad@gmail.com>
2018-03-09 23:07:38 +0800

    去掉不必要的 name 方法

--- types/types.go
+++ types/types.go
@@ -55,9 +55,6 @@ type Engine interface {
 
 // Dialect 接口用于描述与数据库相关的一些语言特性。
 type Dialect interface {
-	// 返回当前数据库的名称。
-	Name() string
-
 	// 返回符合当前数据库规范的引号对。
 	QuoteTuple() (openQuote, closeQuote byte)
 
