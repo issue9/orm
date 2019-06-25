cebcd259c833eb1ef1f4f045b1936cc68a360c2f
caixw <xnotepad@gmail.com>
2019-06-25 23:19:23 +0800

    [model] 去掉唯一约束检测

--- model/models.go
+++ model/models.go
@@ -5,44 +5,20 @@
 package model
 
 import (
-	"errors"
 	"reflect"
 	"sync"
 )
-
-// ErrDuplicateConstraintName 重复的约束名
-//
-// 部分数据需要约束名全局唯一，Models 为了通用性，强制要求全局唯一，
-// 在出现约束名重复时，会返回此错误信息。
-//
-// NOTE: 索引名称也会被作约束名称处理。
-var ErrDuplicateConstraintName = errors.New("已经存在相同的约束名")
 
 // Models 数据模型管理
 type Models struct {
 	locker sync.Mutex
 	items  map[reflect.Type]*Model
-	names  map[string]struct{}
-}
-
-// 保存约束名
-func (ms Models) addNames(name string) error {
-	for key := range ms.names {
-		if key == name {
-			return ErrDuplicateConstraintName
-		}
-	}
-
-	ms.names[name] = struct{}{}
-
-	return nil
 }
 
 // NewModels 声明 Models 变量
 func NewModels() *Models {
 	return &Models{
 		items: map[reflect.Type]*Model{},
-		names: map[string]struct{}{},
 	}
 }
 
