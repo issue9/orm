e86dc028f05f515783bd86a77f74eb7315690ad7
caixw <xnotepad@gmail.com>
2019-07-07 13:46:18 +0800

    [internal/parser] 完善对 sqlite3 的支持

--- internal/parser/create.go
+++ internal/parser/create.go
@@ -5,6 +5,7 @@
 package parser
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 
@@ -24,14 +25,51 @@ type Table struct {
 }
 
 // ParseCreateTable 分析 create table 的语法
-func ParseCreateTable(sql, driverName string) (*Table, error) {
+func ParseCreateTable(driverName, table string, engine sqlbuilder.Engine) (*Table, error) {
 	switch driverName {
 	case "mysql":
-		return parseMysqlCreateTable(filterCreateTableSQL(sql))
+		return getMysqlTableInfo(table, engine)
 	case "sqlite3":
-		return parseSqlite3CreateTable(filterCreateTableSQL(sql))
+		return getSqlite3TableInfo(table, engine)
 	}
 	panic("未实现的数据库：" + driverName)
+}
+
+func getMysqlTableInfo(table string, engine sqlbuilder.Engine) (*Table, error) {
+	rows, err := engine.Query("SHOW CREATE TABLE `" + table + "`")
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err1 := rows.Close(); err1 != nil {
+			err = errors.New(err1.Error() + err.Error())
+		}
+	}()
+
+	var tableName, sql string
+	if err = rows.Scan(&tableName, &sql); err != nil {
+		return nil, err
+	}
+
+	return parseMysqlCreateTable(filterCreateTableSQL(sql))
+}
+
+func getSqlite3TableInfo(tableName string, engine sqlbuilder.Engine) (*Table, error) {
+	table := &Table{
+		Columns:     make(map[string]string, 10),
+		Constraints: make(map[string]sqlbuilder.Constraint, 5),
+		Indexes:     make(map[string]sqlbuilder.Index, 2),
+	}
+
+	if err := parseSqlite3CreateTable(table, tableName, engine); err != nil {
+		return nil, err
+	}
+
+	if err := parseSqlite3Indexes(table, tableName, engine); err != nil {
+		return nil, err
+	}
+
+	return table, nil
 }
 
 // https://dev.mysql.com/doc/refman/8.0/en/create-table.html
@@ -85,12 +123,23 @@ func parseMysqlCreateTable(lines []string) (*Table, error) {
 }
 
 // https://www.sqlite.org/draft/lang_createtable.html
-func parseSqlite3CreateTable(lines []string) (*Table, error) {
-	table := &Table{
-		Columns:     make(map[string]string, len(lines)),
-		Constraints: make(map[string]sqlbuilder.Constraint, len(lines)),
-		Indexes:     make(map[string]sqlbuilder.Index, len(lines)),
+func parseSqlite3CreateTable(table *Table, tableName string, engine sqlbuilder.Engine) error {
+	rows, err := engine.Query("SELECT sql FROM sqlite_master WHERE type='table' and name='" + tableName + "'")
+	if err != nil {
+		return err
 	}
+	defer func() {
+		if err1 := rows.Close(); err1 != nil {
+			err = errors.New(err1.Error() + err.Error())
+		}
+	}()
+
+	var sql string
+	if err = rows.Scan(&sql); err != nil {
+		return err
+	}
+
+	lines := filterCreateTableSQL(sql)
 
 LOOP:
 	for _, line := range lines {
@@ -125,16 +174,35 @@ LOOP:
 			case "FOREIGN":
 				table.Constraints[name] = sqlbuilder.ConstraintFK
 			default:
-				return nil, fmt.Errorf("未知的约束名：%s", line[:index])
+				return fmt.Errorf("未知的约束名：%s", line[:index])
 			}
 		default: // 普通列定义，第一个字符串即为列名
 			table.Columns[line[:index]] = line[index+1:]
 		}
 	}
 
-	// TODO 如何获取索引信息
+	return nil
+}
+func parseSqlite3Indexes(table *Table, tableName string, engine sqlbuilder.Engine) error {
+	rows, err := engine.Query("SELECT name FROM sqlite_master WHERE type='index' AND tbl_name='" + tableName + "'")
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err1 := rows.Close(); err1 != nil {
+			err = errors.New(err1.Error() + err.Error())
+		}
+	}()
 
-	return table, nil
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			return err
+		}
+		table.Indexes[name] = sqlbuilder.IndexDefault
+	}
+
+	return nil
 }
 
 func filterCreateTableSQL(sql string) []string {
