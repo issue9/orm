b3ec3d934c458057f2c7edea94bf42e1c1f5987a
caixw <xnotepad@gmail.com>
2020-01-09 00:13:48 +0800

    test: 修正测试错误

--- core/column_test.go
+++ core/column_test.go
@@ -5,36 +5,10 @@
 package core
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/issue9/assert"
 )
-
-func TestColumn_IsZero(t *testing.T) {
-	a := assert.New(t)
-
-	col, err := NewColumnFromGoType(IntType)
-	a.NotError(err).NotNil(col)
-	a.True(col.IsZero(reflect.ValueOf(int(0))))
-	a.False(col.IsZero(reflect.ValueOf(1)))
-
-	col, err = NewColumnFromGoType(reflect.TypeOf([]byte{}))
-	a.NotError(err).NotNil(col)
-	a.True(col.IsZero(reflect.ValueOf([]byte{})))
-	a.True(col.IsZero(reflect.ValueOf([]byte(""))))
-	a.False(col.IsZero(reflect.ValueOf([]byte{'0'})))
-
-	col, err = NewColumnFromGoType(RawBytesType)
-	a.NotError(err).NotNil(col)
-	a.True(col.IsZero(reflect.ValueOf([]byte{})))
-	a.True(col.IsZero(reflect.ValueOf([]byte(""))))
-	a.False(col.IsZero(reflect.ValueOf([]byte{'0'})))
-	a.False(col.IsZero(reflect.ValueOf(1)))
-
-	col, err = NewColumnFromGoType(reflect.TypeOf(func() {}))
-	a.ErrorType(err, ErrInvalidColumnType).Nil(col)
-}
 
 func TestColumn_Clone(t *testing.T) {
 	a := assert.New(t)
